d0da99695660ec748663d0b0ece201a1f99c1285
1kurs1 <lmtk161044@gmail.com>
2026-02-20 15:26:11 +0300

    regular expression matching

--- regular_expression_matching/main.cpp
+++ regular_expression_matching/main.cpp
@@ -3,22 +3,22 @@
 #include <cstring>
 
 bool isMatch(std::string s, std::string p){
-    int n = s.length(), m = p.length();
-    bool dTable[n+1][m+1];
-    memset(dTable, false, sizeof(dTable));
-    dTable[0][0] = true;
+  int n = s.length(), m = p.length();
+  bool dTable[n+1][m+1];
+  memset(dTable, false, sizeof(dTable));
+  dTable[0][0] = true;
 
-    for(size_t i = 0; i <= n; ++i){
-      for(size_t j = 1; j <= m; ++j){
-        if(p[j-1] == '*') 
-          dTable[i][j] = dTable[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dTable[i-1][j]);
-        else
-          dTable[i][j] = i > 0 && dTable[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
-      }
+  for(size_t i = 0; i <= n; ++i){
+    for(size_t j = 1; j <= m; ++j){
+      if(p[j-1] == '*') 
+        dTable[i][j] = dTable[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dTable[i-1][j]);
+      else
+        dTable[i][j] = i > 0 && dTable[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
     }
-    
-    return dTable[n][m];
   }
+  
+  return dTable[n][m];
+}
 
 int main(){
   std::cout << isMatch("aab", "c*a*b") << '\n';
