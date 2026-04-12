4b50fa160764fe19995d51845f68276299aeebf0
1kurs1 <lmtk161044@gmail.com>
2026-04-12 09:17:22 +0300

    6. Zigzag Conversion

---
+++ 6.ZigzagConversion/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string convert(std::string s, int numRows) {
+    if(numRows == 1 || numRows >= s.length()) return s;
+
+    int idx = 0, d = 1;
+    std::vector<std::vector<char>> rows(numRows);
+
+    for(char c : s){
+        rows[idx].push_back(c);
+        if(idx == 0) d = 1;
+        else if(idx == numRows - 1) d = -1;
+
+        idx += d;
+    }
+
+    std::string result;
+    for(const auto& row: rows){
+        for(char c: row){
+            result += c;
+        }
+    }
+
+    return result;
+}
+
+int main(){
+    return 0;
+}
